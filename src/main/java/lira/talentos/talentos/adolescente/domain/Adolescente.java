abb96c9381d682f02be6ad3bf566720acf0c7359
zeltolira <zelto16@hotmail.com>
2023-08-31 13:04:55 -0300

    implementa adolescente domain

---
+++ src/main/java/lira/talentos/talentos/adolescente/domain/Adolescente.java
@@ -0,0 +1,44 @@
+package lira.talentos.talentos.adolescente.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "adolescente")
+public class Adolescente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idAdolescente;
+	@NotBlank
+	private String nomeAdolescente;
+	@NotNull
+	private LocalDate dataNascimento;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private Sexo sexo;
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraultimaAlteracao;
+	
+}
---
+++ src/main/java/lira/talentos/talentos/adolescente/domain/Sexo.java
@@ -0,0 +1,5 @@
+package lira.talentos.talentos.adolescente.domain;
+
+public enum Sexo {
+	MASCULINO, FEMININO;
+}
